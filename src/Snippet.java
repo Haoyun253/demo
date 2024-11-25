29af28d80d13024fde18b05cfb4e1b39b17bee0f
Haoyun253 <deng0055@algonquinlive.com>
2024-11-25 16:55:45 -0500

    mian6

--- src/Snippet.java
+++ src/Snippet.java
@@ -6,6 +6,16 @@ public class Snippet {
 	private Connection connection;
 	    private void connectToDatabase() {
 	    	
-
+	        String url="jdbc:mysql://localhost:3306/my_db?useUnicode=true&serverTimezone=Asia/Shanghai&characterEncoding=utf8&autoReconnect=true&useSSL=false&allowMultiQueries=true&useAffectedRows=true";
+	        String password = "asd123";
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            connection = DriverManager.getConnection(url,user,password);
+	            System.out.println("Connected to the database successfully.");
+	        } catch (ClassNotFoundException | SQLException e) {
+	            System.err.println("Error connecting to the database.");
+	            e.printStackTrace();
+	            System.err.println("Error connecting to the database."); 
+	        }
 	    }
 }
